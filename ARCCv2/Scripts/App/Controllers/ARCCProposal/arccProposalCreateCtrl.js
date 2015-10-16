c1bcf9b75e36ae84e5daff4c1add1cb5bfb52896
persephione <persephione@gmail.com>
2015-10-16 11:19:33 -0600

    Added hardware budget quick entry

--- ARCCv2/Scripts/App/Controllers/ARCCProposal/arccProposalCreateCtrl.js
+++ ARCCv2/Scripts/App/Controllers/ARCCProposal/arccProposalCreateCtrl.js
@@ -3,13 +3,52 @@
     function ($scope, $filter, $location, arccProposal) {
         $scope.model = {
             proposal: {},
+            hardwareQuickEntry: { hardware: '', arcc: '', department: '', college: '', other: '' },
+            softwareQuickEntry: { software: '', arcc: '', department: '', college: '', other: '' },
+            otherQuickEntry: { other: '', arcc: '', department: '', college: '', other: '' },
+            hardwareList: [],
+            softwareList: [],
+            otherList: [],
             warningText: ''
+        };
+
+        $scope.addHardwareQuickEntry = function () {
+            if (isNotEmpty($scope.model.hardwareQuickEntry)) {
+                $scope.model.hardwareList.push(angular.copy($scope.model.hardwareQuickEntry));
+                $scope.model.hardwareQuickEntry = { hardware: '', arcc: '', department: '', college: '', other: '' };
+            }
+        };
+
+        $scope.addSoftwareQuickEntry = function () {
+            if (isNotEmpty($scope.model.softwareQuickEntry)) {
+                $scope.model.softwareList.push(angular.copy($scope.model.softwareQuickEntry));
+                $scope.model.softwareQuickEntry = { software: '', arcc: '', department: '', college: '', other: '' };
+            }
+        };
+
+        $scope.addOtherQuickEntry = function () {
+            if (isNotEmpty($scope.model.otherQuickEntry)) {
+                $scope.model.otherList.push(angular.copy($scope.model.otherQuickEntry));
+                $scope.model.otherQuickEntry = { other: '', arcc: '', department: '', college: '', other: '' };
+            }
         };
 
         $scope.save = function () {
             arccProposal.SaveOrUpdateARCCProposal.Update($scope.model.proposal).then(function (result) {
                 if (result === 1) { }
-                    // then save was a success, else there was an error and it wasn't saved to db
+                // then save was a success, else there was an error and it wasn't saved to db
             });
+        };
+
+        isNotEmpty = function (quickEntryItem) {
+            var notEmpty = false;
+            for (var key in quickEntryItem) {
+                if (quickEntryItem.hasOwnProperty(key)) {
+                    notEmpty = notEmpty || (!angular.equals(quickEntryItem[key], ''));
+                }
+                if (notEmpty)
+                    break;
+            }
+            return notEmpty;
         };
     }]);
